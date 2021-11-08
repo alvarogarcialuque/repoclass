ffaf1f486706289eb1c5351f8c38703e0e687f75
FranRguez99 <franciscorodriguezespinosa@safareyes.es>
2021-11-08 20:36:17 +0100

    Creación de archivo botarate.py con el menú inicial

---
+++ botarate.py
@@ -0,0 +1,21 @@
+print('\t\t\t\t\t \033[1m\033[4mAPLICACIÓN BOT-ARATE\033[0m')
+
+salir = False
+
+while not salir:
+    print("\n\t\t\t\t\t\t\t\033[1mMenú de Opciones")
+    print("\t\t\t\t\t\t\t================\033[0m\n")
+    print("\t\t\t1) Bot simple (respuestas planas...)")
+    print("\t\t\t2) Bot simple (respuestas REGEX)")
+    print("\t\t\t3) Bot simple mejorado desde fichero")
+    print("\t\t\t4) Informe de la conversación (PDF)")
+    print("\t\t\t5) Salir")
+    opcion = int(input("\n\t\t\t\t\t\t\033[1mOpción : \033[0m"))
+
+    if 1 <= opcion <= 5:
+
+        #Código aquí
+        print("temp")
+
+    else:
+        print("Introduzca un número entre 1 y 5")
